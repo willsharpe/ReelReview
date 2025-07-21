5d332b887064d2737ee6a2cd7e2da32b442da5a9
willsharpe <willsharpe324@gmail.com>
2025-07-21 11:58:21 -0400

    Commit

--- backend/models/movies.js
+++ backend/models/movies.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const movieSchema = new mongoose.Schema({
+  _id:{
+    type:String,
+    required:true,
+  },
   title: {
     type: String,
     required: true,
